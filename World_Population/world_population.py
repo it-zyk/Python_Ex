63598a6b8cafb11c694914946622156e43a06c5f
it-zyk <it-zyk@outlook.com>
2018-10-24 16:58:43 +0800

    分组绘制世界人口地图

--- World_Population/world_population.py
+++ World_Population/world_population.py
@@ -2,10 +2,13 @@
 
 import json
 from country_codes import get_country_code
-
+import pygal.maps.world
+from pygal.style import LightColorizedStyle as LCS, RotateStyle as RS
 
 # 将数据加载到一个列表中
 filename = 'population_data.json'
+cc_populations = {}
+
 with open(filename) as f:
     pop_data = json.load(f)
     # 打印每个国家2010年的人口数量
@@ -15,6 +18,29 @@ with open(filename) as f:
             population = int(float(pop_dict['Value']))
             code = get_country_code(country_name)
             if code:
-                print(code + ": " + str(population))
-            else:
-                print("Error - " + country_name)
+                cc_populations[code] = population
+            #     print(code + ": " + str(population))
+            # else:
+            #     print("Error - " + country_name)
+    # print(cc_populations)
+
+cc_pops_1, cc_pops_2, cc_pops_3 = {}, {}, {}
+for cc, pop in cc_populations.items():
+    if pop < 10000000:
+        cc_pops_1[cc] = pop
+    elif pop < 1000000000:
+        cc_pops_2[cc] = pop
+    else:
+        cc_pops_3[cc] = pop
+# print(len(cc_pops_1), len(cc_pops_2), len(cc_pops_3))
+# wm_style = RotateStyle('#336699')
+# wm_style = LightColorizedStyle
+wm_style = RS('#336699', base_style=LCS)
+wm = pygal.maps.world.World(style=wm_style)
+
+wm.title = 'World Population in 2010, by Country'
+wm.add('0-10m', cc_pops_1)
+wm.add('10m-1bn', cc_pops_2)
+wm.add('>1bn', cc_pops_3)
+#wm.add('2010', cc_populations)
+wm.render_to_file('world_population.svg')
