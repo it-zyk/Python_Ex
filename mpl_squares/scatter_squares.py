8ae03873ec7aa889c5c2ef3b2d65903bc21e876d
it-zyk <it-zyk@outlook.com>
2018-12-21 11:17:32 +0800

    使用Scatter()绘制一系列点

--- mpl_squares/scatter_squares.py
+++ mpl_squares/scatter_squares.py
@@ -2,6 +2,8 @@
 
 import matplotlib.pyplot as plt
 
+
+# ----------------------使用Scatter()绘制一系列点----
 # x_value = [1, 2, 3, 4, 5]
 # y_value = [1, 4, 9, 16, 25]
 x_value = list(range(1, 1001))
@@ -19,7 +21,7 @@ plt.ylabel("Square of Value", fontsize=14)
 # 设置每个坐标轴的取值范围
 plt.axis([0, 1100, 0, 1100000])
 # 设置刻度标记的大小
-# plt.tick_params(axis='both', which='major', labelsize=14)
+plt.tick_params(axis='both', which='major', labelsize=14)
 
-
+plt.savefig('square_png',bbox_inches='tight')
 plt.show()
